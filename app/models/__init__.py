39302e95610f5ba0f7db671d3b5130fbf65ff72e
Jeff Steele <steelem81@gmail.com>
2026-03-01 17:14:39 -0800

    Added resource progress model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,11 +3,13 @@ from .learning_path import LearningPath
 from .module import Module
 from .learning_resource import LearningResource
 from .schedule import Schedule
+from .resource_progress import ResourceProgress
 
 __all__ = [
     "Base",
     "LearningPath",
     "Module",
     "LearningResource",
-    "Schedule"
+    "Schedule",
+    "ResourceProgress"
 ]
